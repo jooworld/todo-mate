5e07a10a6b8079b7ba3e41bc8c32be5688394789
jooworld <jooworld396@gmail.com>
2021-11-21 13:58:46 +0900

    Feed 파일 category 부분 수정

--- src/Feed.js
+++ src/Feed.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Feed.css';
-import { MdAddCircleOutline } from 'react-icons/md';
+import { MdAddCircleOutline, MdInventory } from 'react-icons/md';
 
 const Feed = () => {
     return (
         <div>
             <h1 className="Feed">Feed</h1>
 
-            <button>
-                <span style={{ backgroundColor: "red" }}>일반</span>
-                <MdAddCircleOutline size="20" style={{ backgroundColor: "green" }} />
+            <button className="btn">
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <MdInventory size="20" color="white" />
+                    <h3 className="category">일반</h3>
+                    <MdAddCircleOutline size="17" color="white" />
+                </div>
             </button>
         </div>
     );
